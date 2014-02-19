14ba108439276d0a631cf33bfe67314f5e579d6b
Michael Wiss <michael.wiss@gmail.com>
2014-02-18 19:14:18 -0500

    updated debouncedresize.js in grid.js

--- js/build/production.js
+++ js/build/production.js
@@ -59,33 +59,30 @@ function FastClick(a){"use strict";var b,c=this;if(this.trackingClick=!1,this.tr
 var $event = $.event,
 $special,
 resizeTimeout;
-
 $special = $event.special.debouncedresize = {
-	setup: function() {
-		$( this ).on( "resize", $special.handler );
-	},
-	teardown: function() {
-		$( this ).off( "resize", $special.handler );
-	},
-	handler: function( event, execAsap ) {
-		// Save the context
-		var context = this,
-			args = arguments,
-			dispatch = function() {
-				// set correct event type
-				event.type = "debouncedresize";
-				$event.dispatch.apply( context, args );
-			};
-
-		if ( resizeTimeout ) {
-			clearTimeout( resizeTimeout );
-		}
-
-		execAsap ?
-			dispatch() :
-			resizeTimeout = setTimeout( dispatch, $special.threshold );
-	},
-	threshold: 250
+setup: function() {
+$( this ).on( "resize", $special.handler );
+},
+teardown: function() {
+$( this ).off( "resize", $special.handler );
+},
+handler: function( event, execAsap ) {
+// Save the context
+var context = this,
+args = arguments,
+dispatch = function() {
+// set correct event type
+event.type = "debouncedresize";
+$event.dispatch.apply( context, args );
+};
+if ( resizeTimeout ) {
+clearTimeout( resizeTimeout );
+}
+execAsap ?
+dispatch() :
+resizeTimeout = setTimeout( dispatch, $special.threshold );
+},
+threshold: 150
 };
 
 // ======================= imagesLoaded Plugin ===============================
